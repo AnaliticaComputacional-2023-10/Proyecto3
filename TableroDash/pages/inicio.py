86951e1d1e5dd18726c92105c646f4f394b1d539
Julicardeb <cardenasjulianacarolina@gmail.com>
2023-05-19 13:38:39 -0500

    Cambios

--- TableroDash/pages/inicio.py
+++ TableroDash/pages/inicio.py
@@ -2,16 +2,7 @@ from dash import html
 from apps import navigation
 import dash_bootstrap_components as dbc
 
-texto = '''Las enfermedades cardíacas son una de las principales causas de muerte en todo el mundo.
-Estas enfermedades afectan al corazón y al sistema circulatorio, lo que puede llevar a complicaciones
-graves, como ataques cardíacos y accidentes cerebrovasculares.
-El siguiente programa ha sido desarrollado como apoyo a médicos en el proceso de evaluación de
-pacientes para detectar si una persona tiene o no una enfermedad cardíaca y la toma de decisiones
-asociada como solicitud de exámenes, chequeos y otros procedimientos.
-Este programa se ha desarrollado mediante la exploración de datos y el modelado con
-redes bayesianas, con el fin de descubrir la relación entre 14 atributos relevantes en la base
-de datos de enfermedades cardíacas de Cleveland. Los datos se recopilaron del repositorio de la
-Universidad de California en Irvine.'''
+texto = '''  '''
 
 texto3 = 'Aplicación creada por Santiago González y Juliana Cárdenas.'
 
