16bc6ef4504149d759d650b344f17d4e0f2df89c
SantiGonzalezz <s.gonzalez35@uniandes.edu.co>
2023-05-31 23:39:55 -0500

    Graficas terminadas

--- Data/Scripts/Zona_Script.sql
+++ Data/Scripts/Zona_Script.sql
@@ -1,8 +1,11 @@
 
 CREATE TABLE promedio_zona (
     zona VARCHAR(255),
+    tipo VARCHAR(255),
     punt_promedio NUMBER
 );
-INSERT INTO promedio_zona (zona, punt_promedio) VALUES
-(RURAL, 238.869597458878),
-(URBANO, 257.86885741837335);
+INSERT INTO promedio_zona (zona, tipo, punt_promedio) VALUES
+('Rural', 'Privado', 302.4471307742355),
+('Rural', 'Publico', 232.52439417157996),
+('Urbano', 'Privado', 277.03880722298237),
+('Urbano', 'Publico', 250.85332804547556);
