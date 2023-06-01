ba07d53b6c50a190df7a94c9014412d29bb2cda5
SantiGonzalezz <s.gonzalez35@uniandes.edu.co>
2023-05-31 22:08:24 -0500

    Notebook zona

---
+++ Data/Scripts/Zona_Script.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE promedio_zona (
+    zona VARCHAR(255),
+    punt_promedio NUMBER
+);
+INSERT INTO promedio_zona (zona, punt_promedio) VALUES
+(RURAL, 238.869597458878),
+(URBANO, 257.86885741837335);
